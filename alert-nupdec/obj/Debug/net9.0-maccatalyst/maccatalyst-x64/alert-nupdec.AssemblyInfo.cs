5f263d276e924c33712bbf2eaa5cd7b4c79611f8
Iago Lima <iagolima40028922@gmail.com>
2025-10-26 17:04:54 -0300

    commit para merge com login

--- alert-nupdec/obj/Debug/net9.0-maccatalyst/maccatalyst-x64/alert-nupdec.AssemblyInfo.cs
+++ alert-nupdec/obj/Debug/net9.0-maccatalyst/maccatalyst-x64/alert-nupdec.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("alert-nupdec")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3bb1bfd927ac7df7615b256d0f6115af596bd46f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5c63092c30437fcf9b726f161515e05d67f0a312")]
 [assembly: System.Reflection.AssemblyProductAttribute("alert-nupdec")]
 [assembly: System.Reflection.AssemblyTitleAttribute("alert-nupdec")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
